8b99efb5e77ec0de825b1e5a0c3adaa1849b6833
Alkcode <alek_1604@comunidad.unam.mx>
2021-05-28 00:28:34 -0500

    Agregamos pagina de mario cabral pes

---
+++ mario-cabral-pes/js/index.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal();
+
+
+sr.reveal('.reg',{
+    duration:4000,
+    origin:'top',
+    distance:'-100px'
+});
+
+sr.reveal('.parrafo',{
+    duration:1000,
+    origin:'top',
+    distance:'-100px'
+});
+
+
+sr.reveal('.target',{
+    duration:4000,
+    origin:'button',
+    distance:'-100px'
+});
