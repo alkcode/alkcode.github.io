181b6b33e3175fa4f61775c4395ba875d835c9ca
alkcode <alek_1604@comunidad.unam.mx>
2022-08-18 23:13:14 -0500

    Pagina web CECIM

--- CECIM/js/scripts.js
+++ CECIM/js/scripts.js
@@ -51,38 +51,38 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    //Glider
-    new Glider(document.querySelector('.carousel__lista'), {
-		slidesToShow: 1,
-		slidesToScroll: 1,
-        rewind: true,
-        draggable: true,
-		dots: '.carousel__indicadores',
-		arrows: {
-			prev: '.carousel__anterior',
-			next: '.carousel__siguiente'
-		},
-		responsive: [
-			{
-			// screens greater than >= 775px
-		    breakpoint: 450,
-			settings: {
-			    // Set to `auto` and provide item width to adjust to viewport
-				slidesToShow: 2,
-				slidesToScroll: 2,
-                rewind: true,
-                draggable: true,
-			}
-			},{
-			  // screens greater than >= 1024px
-			breakpoint: 800,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3,
-                rewind: true,
-                draggable: true,
-			}
-			}
-		]
-	});
+    // //Glider
+    // new Glider(document.querySelector('.carousel__lista'), {
+	// 	slidesToShow: 1,
+	// 	slidesToScroll: 1,
+    //     rewind: true,
+    //     draggable: true,
+	// 	dots: '.carousel__indicadores',
+	// 	arrows: {
+	// 		prev: '.carousel__anterior',
+	// 		next: '.carousel__siguiente'
+	// 	},
+	// 	responsive: [
+	// 		{
+	// 		// screens greater than >= 775px
+	// 	    breakpoint: 450,
+	// 		settings: {
+	// 		    // Set to `auto` and provide item width to adjust to viewport
+	// 			slidesToShow: 2,
+	// 			slidesToScroll: 2,
+    //             rewind: true,
+    //             draggable: true,
+	// 		}
+	// 		},{
+	// 		  // screens greater than >= 1024px
+	// 		breakpoint: 800,
+	// 		settings: {
+	// 			slidesToShow: 3,
+	// 			slidesToScroll: 3,
+    //             rewind: true,
+    //             draggable: true,
+	// 		}
+	// 		}
+	// 	]
+	// });
 });
